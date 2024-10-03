ccddc6c3077f28f000d9f7f52906309e54fd0994
Syed Safwan <91465866+safwan124@users.noreply.github.com>
2024-10-03 23:36:29 +0530

    url changes

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={"/boyle/"}>
+    <BrowserRouter basename={"/bolye/"}>
       <App />
     </BrowserRouter>
   </StrictMode>,
