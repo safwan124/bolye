3ee6263cc9b4a41b36783fa2fa9c44d14314855c
Syed Safwan <91465866+safwan124@users.noreply.github.com>
2024-11-06 22:01:42 +0530

    switched to dark theme

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -20,7 +20,7 @@ const AboutUs = () => {
       <Row>
         <Col className="mx-auto">
           <p className="offer-text" >
-          At <span className="bold-text">BOYLE LIFESCIENCE</span>, we offer innovative cosmetic solutions tailored to your brand's needs. Our range includes premium skincare, hair care, and beauty products, all crafted with high quality, ethically sourced ingredients. We provide comprehensive private labeling services to help you create a unique brand identity. Our R&D team is dedicated to developing cutting edge formulations that meet the latest beauty trends. We ensure stringent quality control to deliver safe, effective products. Partner with us for customized manufacturing solutions that elevate your brand in the competitive cosmetic market.
+          At <span className="bold-text offer-text">BOYLE LIFESCIENCE</span>, we offer innovative cosmetic solutions tailored to your brand's needs. Our range includes premium skincare, hair care, and beauty products, all crafted with high quality, ethically sourced ingredients. We provide comprehensive private labeling services to help you create a unique brand identity. Our R&D team is dedicated to developing cutting edge formulations that meet the latest beauty trends. We ensure stringent quality control to deliver safe, effective products. Partner with us for customized manufacturing solutions that elevate your brand in the competitive cosmetic market.
           </p>
         </Col>
       </Row>
